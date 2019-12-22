b42dcbda4bc75e712eb2b426206aa8364fed2bb2
kaushik mishra <kaushik.mishra@srijan.net>
2019-12-22 21:02:04 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React here
+          Learn React
         </a>
       </header>
     </div>
